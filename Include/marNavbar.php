e490c788846c0f16d85ce369742a88b611c4b0ce
Dagtwice <eduarddan_02@yahoo.com>
2019-10-27 17:09:01 +0800

    Up

--- Include/marNavbar.php
+++ Include/marNavbar.php
@@ -22,7 +22,7 @@
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Programs
+                    Program
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="MarProgArcdp.php">ARCDP</a>
